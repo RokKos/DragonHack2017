cefc051303eeae617a2a3574ec02aa122d0c48fc
bor brecelj <bor.brecelj@gmail.com>
2017-05-21 01:05:27 +0200

    Start creating start menu

---
+++ Assets/Scripts/start_menu_script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start_menu_script : MonoBehaviour {
+
+    [SerializeField] Transform panelBoxes;
+    [SerializeField] Transform startMenuPanel;
+
+    // Use this for initialization
+    void Start () {
+        panelBoxes.gameObject.SetActive(false);
+        startMenuPanel.gameObject.SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
