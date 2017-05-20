ed2ce1a584b9993929176a2b4357f0ed3ff270cc
bor brecelj <bor.brecelj@gmail.com>
2017-05-20 15:59:11 +0200

    Random tests

--- Assets/Scripts/box_script.cs
+++ Assets/Scripts/box_script.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class box_script : MonoBehaviour {
 
+    [SerializeField] Sprite[] images;
+
+    Image myImage;
+    int stanje = 0;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        myImage = GetComponent<Image>();
+        myImage.sprite = images[0];
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
