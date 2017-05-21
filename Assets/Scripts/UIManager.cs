b4958cc354c35b3ad4a1337fce7f9458c084c933
RokKos <rocki5555@gmail.com>
2017-05-21 03:38:51 +0200

    Start Menu

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,13 @@
-﻿using System.Collections;
+﻿//Author: Rok Kos <kosrok97@gmail.com>
+//File: UIManager.cs
+//File path: /D/Documents/Unity/DragonHack2017/UIManager.cs
+//Date: 21.05.2017
+//Description: UI Functions
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour {
 
@@ -9,12 +16,16 @@ public class UIManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        panelBoxes.gameObject.SetActive(false);
-        startMenuPanel.gameObject.SetActive(true);
+        //panelBoxes.gameObject.SetActive(false);
+        //startMenuPanel.gameObject.SetActive(true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void gotoMain () {
+        SceneManager.LoadScene("Main");
+    }
 }
